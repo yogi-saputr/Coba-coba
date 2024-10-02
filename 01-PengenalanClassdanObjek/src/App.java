5ec51ebdfe3f62d1c3f2faf64fc2d972b54e41db
yogi-saputr <145946382+yogi-saputr@users.noreply.github.com>
2024-10-02 14:36:23 +0800

    Modul 1

---
+++ 01-PengenalanClassdanObjek/src/App.java
@@ -0,0 +1,16 @@
+// Membuat class sebagai template
+
+class Mahasiswa{
+    // Ciri-ciri atau atribut objek
+    String nama;
+    String NIM;
+    String jurusan;
+    double IPK;
+    int umur;
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
